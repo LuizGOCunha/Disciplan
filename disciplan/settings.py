25c89c242c035209863262023570087b69c802b7
Luiz <valuiz1@gmail.com>
2023-03-28 16:51:36 -0300

    Connected project to postgresql db

--- disciplan/settings.py
+++ disciplan/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = "disciplan.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "disciplandb",
+        'USER': 'postgres',
+        'PASSWORD': '789456123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
