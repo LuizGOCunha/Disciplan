d9a14a6ff08904fb2560196c6b8d28893601d69e
Luiz <valuiz1@gmail.com>
2023-03-29 16:08:43 -0300

    Added requirements.txt, hid django key

--- website/models.py
+++ website/models.py
@@ -4,23 +4,28 @@ from django.contrib.auth.models import AbstractBaseUser
 
 # Create your models here.
 
+
 class User(AbstractBaseUser):
-    first_name = models.CharField("First Name", max_length=100,)
+    first_name = models.CharField(
+        "First Name",
+        max_length=100,
+    )
     last_name = models.CharField("Second Name", max_length=100)
     email = models.EmailField(unique=True)
 
-    USERNAME_FIELD = 'email'
+    USERNAME_FIELD = "email"
 
     def __str__(self) -> str:
         return f"{self.first_name} | {self.email}"
 
+
 class Activity(models.Model):
     DIFFICULTY_CHOICES = (
-        (100, 'very easy'),
-        (200, 'easy'),
-        (300, 'medium'),
-        (400, 'hard'),
-        (500, 'very hard')
+        (100, "very easy"),
+        (200, "easy"),
+        (300, "medium"),
+        (400, "hard"),
+        (500, "very hard"),
     )
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
@@ -29,4 +34,3 @@ class Activity(models.Model):
 
     def __str__(self) -> str:
         return self.title
-    
